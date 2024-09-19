6ec3de7cc7b0e9892fc581c01c8980f5a960b19d
Anamika1608 <anamikaagg18@gmail.com>
2024-09-19 12:40:06 +0530

    Hero Page Animation

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-
+import Stamps from './Stamps';
 function Hero() {
 
   const stamps = [
@@ -23,25 +23,28 @@ function Hero() {
 
 
   return (
-    <div className="bg-[#F5F4E8] p-4">
+    <div className="bg-[#F5F4E8] p-4 pb-32">
       <main className="flex">
-        <div className="w-2/3 pr-8 pl-14">
+        <div className="w-2/3 pr-8 pl-24">
           <div className="text-6xl font-serif text-[#4C1F1F] mb-4 young-serif-regular">
             Explore the
             <div className="pt-3">collection of</div>
             <div className="pt-3">Popular vintage</div>
             <div className="pt-2">stamps</div>
           </div>
+
           <div className="text-[#4C1F1F] mt-10 text-2xl ibm-plex-mono-regular">
             Showcasing the societal and<br />
             architectural heritage of different<br />
             eras.
           </div>
         </div>
-
-        <div className=" bg-white rounded-lg p-52 shadow-md">
-         
+        <div className='mt-32 p-28'>
+          <Stamps />
         </div>
+
+
+
       </main>
     </div>
   );
