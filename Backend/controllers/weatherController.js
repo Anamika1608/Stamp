a9408274ad7ea687967613f2aff3226e86e78015
Anamika Aggarwal <anamikaagg18@gmail.com>
2024-11-25 10:32:47 +0530

    final frontend changes

--- Backend/controllers/weatherController.js
+++
@@ -1,5 +0,0 @@
-export const weatherForecast = (req,res)=>{
-    const {location } = req.body
-    console.log(location);
-    res.status(200).json(location);
-}
--- Backend/routes/weatherRoute.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-import {weatherForecast} from '../controllers/weatherController.js'
-
-router.get('/weather',weatherForecast);
-
-export default router;
